714588d52b9818f02d90a860f64ed6f863f9d0f8
guowei <guowzou@gmail.com>
2019-07-14 17:44:18 +0800

    add test

--- TwoPointer/ThreeSum.cpp
+++ TwoPointer/ThreeSum.cpp
@@ -7,86 +7,127 @@
 #include <set>
 #include <algorithm>
 
-class Solution {
-public:
-    /**
-     * @param numbers: Give an array numbers of n integer
-     * @return: Find all unique triplets in the array which gives the sum of zero.
-     */
-    std::vector<std::vector<int>> threeSum(std::vector<int> &numbers) {
-        // write your code here
-        std::vector<std::vector<int>> result;
-        std::sort(numbers.begin(), numbers.end());
-        std::set<std::pair<int, int>> uniquePairSet;
+#include "TwoPointers.hpp"
 
-        if (numbers.size() >= 3) {
+std::vector<std::vector<int>> ThreeSum::threeSum(std::vector<int> &numbers) {
+    // write your code here
+    std::vector<std::vector<int>> result;
+    std::sort(numbers.begin(), numbers.end());
+    std::set<std::pair<int, int>> uniquePairSet;
 
-            int total = numbers.size();
-            for (int index = 0; index <= total - 3; index++) {
+    if (numbers.size() >= 3) {
 
-                int start = index + 1;
-                int end = numbers.size() - 1;
+        int total = numbers.size();
+        for (int index = 0; index <= total - 3; index++) {
 
-                while (start < end) {
+            int start = index + 1;
+            int end = numbers.size() - 1;
 
-                    int sum = numbers[start] + numbers[end] + numbers[index];
-                    if (sum == 0) {
-                        auto pair = std::make_pair(numbers[index], numbers[start]);
-                        if (uniquePairSet.find(pair) == uniquePairSet.end()) {
-                            auto three = std::vector<int>{numbers[index], numbers[start], numbers[end]};
-                            result.push_back(three);
-                            uniquePairSet.insert(pair);
-                        }
-                        start++;
-                        end--;
-                    } else if (sum > 0) {
-                        end--;
-                    } else {
-                        start++;
+            while (start < end) {
+
+                int sum = numbers[start] + numbers[end] + numbers[index];
+                if (sum == 0) {
+                    auto pair = std::make_pair(numbers[index], numbers[start]);
+                    if (uniquePairSet.find(pair) == uniquePairSet.end()) {
+                        auto three = std::vector<int>{numbers[index], numbers[start], numbers[end]};
+                        result.push_back(three);
+                        uniquePairSet.insert(pair);
                     }
+                    start++;
+                    end--;
+                } else if (sum > 0) {
+                    end--;
+                } else {
+                    start++;
                 }
-
             }
-        }
 
-        return result;
-
-    }
-};
-
-int main(int argc, char *argv[])
-{
-
-    {
-        int array[] = {2, 7, 11, 15};
-        std::vector<int> input(std::begin(array), std::end(array));
-        Solution solution;
-
-        auto results = solution.threeSum(input);
-
-        for (auto &result : results) {
-            for (auto &num : result) {
-                std::cout << num << "\t";
-            }
-            std::cout << std::endl;
         }
     }
 
-    {
-        int array[] = {-1, 0, 1, 2, -1, -4};
-        std::vector<int> input(std::begin(array), std::end(array));
-        Solution solution;
-
-        auto results = solution.threeSum(input);
-
-        for (auto &result : results) {
-            for (auto &num : result) {
-                std::cout << num << "\t";
-            }
-            std::cout << std::endl;
-        }
-    }
-
-
-    return 0;
+    return result;
 }
+
+//class ThreeSum {
+//public:
+//    /**
+//     * @param numbers: Give an array numbers of n integer
+//     * @return: Find all unique triplets in the array which gives the sum of zero.
+//     */
+//    std::vector<std::vector<int>> threeSum(std::vector<int> &numbers) {
+//        // write your code here
+//        std::vector<std::vector<int>> result;
+//        std::sort(numbers.begin(), numbers.end());
+//        std::set<std::pair<int, int>> uniquePairSet;
+//
+//        if (numbers.size() >= 3) {
+//
+//            int total = numbers.size();
+//            for (int index = 0; index <= total - 3; index++) {
+//
+//                int start = index + 1;
+//                int end = numbers.size() - 1;
+//
+//                while (start < end) {
+//
+//                    int sum = numbers[start] + numbers[end] + numbers[index];
+//                    if (sum == 0) {
+//                        auto pair = std::make_pair(numbers[index], numbers[start]);
+//                        if (uniquePairSet.find(pair) == uniquePairSet.end()) {
+//                            auto three = std::vector<int>{numbers[index], numbers[start], numbers[end]};
+//                            result.push_back(three);
+//                            uniquePairSet.insert(pair);
+//                        }
+//                        start++;
+//                        end--;
+//                    } else if (sum > 0) {
+//                        end--;
+//                    } else {
+//                        start++;
+//                    }
+//                }
+//
+//            }
+//        }
+//
+//        return result;
+//
+//    }
+//};
+
+//int main(int argc, char *argv[])
+//{
+//
+//    {
+//        int array[] = {2, 7, 11, 15};
+//        std::vector<int> input(std::begin(array), std::end(array));
+//        ThreeSum solution;
+//
+//        auto results = solution.threeSum(input);
+//
+//        for (auto &result : results) {
+//            for (auto &num : result) {
+//                std::cout << num << "\t";
+//            }
+//            std::cout << std::endl;
+//        }
+//    }
+//
+//    {
+//        int array[] = {-1, 0, 1, 2, -1, -4};
+//        std::vector<int> input(std::begin(array), std::end(array));
+//        ThreeSum solution;
+//
+//        auto results = solution.threeSum(input);
+//
+//        for (auto &result : results) {
+//            for (auto &num : result) {
+//                std::cout << num << "\t";
+//            }
+//            std::cout << std::endl;
+//        }
+//    }
+//
+//
+//    return 0;
+//}
