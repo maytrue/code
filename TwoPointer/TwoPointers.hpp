9de132d28774f776d41d0b4ecdf87ee33026fdbc
guowei <guowzou@gmail.com>
2019-07-14 17:48:52 +0800

    add google test

---
+++ TwoPointer/TwoPointers.hpp
@@ -0,0 +1,19 @@
+//
+// Created by zouguowei on 2019/7/14.
+//
+
+#ifndef PROJECT_TWOPOINTERS_HPP
+#define PROJECT_TWOPOINTERS_HPP
+
+#include <vector>
+
+class ThreeSum {
+public:
+    /**
+     * @param numbers: Give an array numbers of n integer
+     * @return: Find all unique triplets in the array which gives the sum of zero.
+     */
+    std::vector<std::vector<int>> threeSum(std::vector<int> &numbers);
+};
+
+#endif //PROJECT_TWOPOINTERS_HPP
---
+++ test/ThreeSumTest.cpp
@@ -0,0 +1,24 @@
+//
+// Created by zouguowei on 2019/7/14.
+//
+
+#include <gtest/gtest.h>
+#include "TwoPointers.hpp"
+
+TEST(TwoPointer, ThreeSum) {
+    int array[] = {-1, 0, 1, 2, -1, -4};
+    std::vector<int> input(std::begin(array), std::end(array));
+    ThreeSum solution;
+
+    auto results = solution.threeSum(input);
+//    for (auto &result : results) {
+//        for (auto &num : result) {
+//            std::cout << num << "\t";
+//        }
+//        std::cout << std::endl;
+//    }
+
+    std::cout << "test two pointer" << std::endl;
+    ASSERT_EQ(results.size(), 2);
+
+}
