42cba117d8ade02befe39ca09347871767b32fe9
guowei <guowzou@gmail.com>
2019-07-25 08:20:10 +0800

    temporary commit

--- BFS/BreadthFirst.hpp
+++ BFS/BreadthFirst.hpp
@@ -6,7 +6,7 @@
 #define PROJECT_BREADTHFIRST_HPP
 
 #include <vector>
-
+#include <string>
 
 class TreeNode {
 public:
@@ -24,6 +24,27 @@ public:
     std::vector<std::vector<int>> levelOrder(TreeNode * root);
 
     std::vector<std::vector<int>> levelOrderV2(TreeNode * root);
+};
+
+class BinaryTreeRepresent
+{
+public:
+
+    /**
+ * This method will be invoked first, you should design your own algorithm
+ * to serialize a binary tree which denote by a root node to a string which
+ * can be easily deserialized by your own "deserialize" method later.
+ */
+    std::string serialize(TreeNode * root);
+
+    /**
+     * This method will be invoked second, the argument data is what exactly
+     * you serialized at method "serialize", that means the data is not given by
+     * system, it's given by your own serialize method. So the format of data is
+     * designed by yourself, and deserialize it here as you serialize it in
+     * "serialize" method.
+     */
+    TreeNode * deserialize(std::string &data);
 };
 
 #endif //PROJECT_BREADTHFIRST_HPP
--- test/BFSTest.cpp
+++ test/BFSTest.cpp
@@ -6,40 +6,80 @@
 #include "BreadthFirst.hpp"
 
 TEST(BFS, BinaryTreeLevel) {
+//    {
+//        TreeNode *node = new TreeNode(1);
+//        TreeNode *left = new TreeNode(2);
+//        TreeNode *right = new TreeNode(3);
+//        node->left = left;
+//        node->right = right;
+//
+//        BinaryTreeLevelTraversal solution;
+//        auto results = solution.levelOrderV2(node);
+//        for (auto &result : results) {
+//            for (auto &item : result) {
+//                std::cout << item << " ";
+//            }
+//            std::cout << std::endl;
+//        }
+//    }
+//
+//    {
+//        TreeNode *node = new TreeNode(1);
+//        TreeNode *right = new TreeNode(2);
+//        node->left = nullptr;
+//        node->right = right;
+//
+//        right->left = nullptr;
+//        right->right = new TreeNode(3);
+//
+//        BinaryTreeLevelTraversal solution;
+//        auto results = solution.levelOrderV2(node);
+//        for (auto &result : results) {
+//            for (auto &item : result) {
+//                std::cout << item << " ";
+//            }
+//            std::cout << std::endl;
+//        }
+//    }
+//
+//    {
+//        TreeNode *node = new TreeNode(1);
+//        TreeNode *left = new TreeNode(2);
+//        node->left = left;
+//        node->right = nullptr;
+//
+//        left->left = new TreeNode(3);
+//        left->right = nullptr;
+//
+//        BinaryTreeLevelTraversal solution;
+//        auto results = solution.levelOrderV2(node);
+//        for (auto &result : results) {
+//            for (auto &item : result) {
+//                std::cout << item << " ";
+//            }
+//            std::cout << std::endl;
+//        }
+//    }
+}
+
+TEST(BFS, BinaryTreeRepresent) {
+
     {
-        TreeNode *node = new TreeNode(1);
-        TreeNode *left = new TreeNode(2);
-        TreeNode *right = new TreeNode(3);
+        TreeNode *node = new TreeNode(3);
+        TreeNode *left = new TreeNode(9);
+        TreeNode *right = new TreeNode(20);
         node->left = left;
         node->right = right;
 
-        BinaryTreeLevelTraversal solution;
-        auto results = solution.levelOrderV2(node);
-        for (auto &result : results) {
-            for (auto &item : result) {
-                std::cout << item << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
+        node->left->left = nullptr;
+        node->left->right = nullptr;
 
-    {
-        TreeNode *node = new TreeNode(1);
-        TreeNode *right = new TreeNode(2);
-        node->left = nullptr;
-        node->right = right;
+        node->right->left = new TreeNode(15);
+        node->right->right = new TreeNode(7);
 
-        right->left = nullptr;
-        right->right = new TreeNode(3);
-
-        BinaryTreeLevelTraversal solution;
-        auto results = solution.levelOrderV2(node);
-        for (auto &result : results) {
-            for (auto &item : result) {
-                std::cout << item << " ";
-            }
-            std::cout << std::endl;
-        }
+        BinaryTreeRepresent solution;
+        auto result = solution.serialize(node);
+        std::cout << result << std::endl;
     }
 
     {
@@ -48,16 +88,18 @@ TEST(BFS, BinaryTreeLevel) {
         node->left = left;
         node->right = nullptr;
 
-        left->left = new TreeNode(3);
-        left->right = nullptr;
+        node->left->left = new TreeNode(3);
+        node->left->right = new TreeNode(4);
 
-        BinaryTreeLevelTraversal solution;
-        auto results = solution.levelOrderV2(node);
-        for (auto &result : results) {
-            for (auto &item : result) {
-                std::cout << item << " ";
-            }
-            std::cout << std::endl;
-        }
+        BinaryTreeRepresent solution;
+        auto result = solution.serialize(node);
+        std::cout << result << std::endl;
     }
+
+    {
+        std::string input = "3,9,20,#,#,15,7";
+        BinaryTreeRepresent solution;
+        solution.deserialize(input);
+    }
+
 }
